75df11dfcfaef6a0c661ab21421395047f56eb5a
GeneralNel <mushfiqarashid@gmail.com>
2021-05-09 18:58:59 -0500

    Fix null error

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,26 +5,27 @@ const MovieList = (props) => {
   if (props) {
     return (
       <>
-        {props.movies.map((movie, index) => (
-          <div className="movie-container d-flex flex-column justify-content-start m-3">
-            <div className="image-container">
-              <div
-                onClick={() => props.handleNominationsClick(movie)}
-                className="overlay d-flex align-items-center justify-content-center"
-              >
-                <NominationComponent />
-              </div>
-              <img
-                src={movie.Poster}
-                onerror="this.onerror=null; this.src='No-Image-Placeholder.png'"
-              ></img>
-              <div className="text-container">
-                <h3 className="title-header">{movie.Title}</h3>
-                <h4 className="year-text">{movie.Year}</h4>
+        {props.movies &&
+          props.movies.map((movie, index) => (
+            <div className="movie-container d-flex flex-column justify-content-start m-3">
+              <div className="image-container">
+                <div
+                  onClick={() => props.handleNominationsClick(movie)}
+                  className="overlay d-flex align-items-center justify-content-center"
+                >
+                  <NominationComponent />
+                </div>
+                <img
+                  src={movie.Poster}
+                  onerror="this.onerror=null; this.src='No-Image-Placeholder.png'"
+                ></img>
+                <div className="text-container">
+                  <h3 className="title-header">{movie.Title}</h3>
+                  <h4 className="year-text">{movie.Year}</h4>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </>
     );
   }
